049ef88b538a1d7c106c503c229679a2d9a120cd
MikkoHimanka <mikko.himanka@protonmail.com>
2021-02-05 12:13:16 +0200

    Viikko 2, tehtava 2, bugfix

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -24,6 +24,7 @@ public class Varasto {
         }
         if (alkuSaldo < 0.0) {
             this.saldo = 0.0;
+        } else {
             this.saldo = Math.min(alkuSaldo, tilavuus);
         }
     }
